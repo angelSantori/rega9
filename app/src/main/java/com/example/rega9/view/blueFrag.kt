10117f58613cd5067a40bfcab5a3cf8764fc5c85
angelSantori <antonyo.da.mm@gmail.com>
2023-05-19 11:21:35 -0700

    temporizadorFrag.kt agregado, el tiempo incluido está en segundos.

--- app/src/main/java/com/example/rega9/view/blueFrag.kt
+++ app/src/main/java/com/example/rega9/view/blueFrag.kt
@@ -24,6 +24,10 @@ import androidx.activity.result.ActivityResultLauncher
 
 //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 import android.Manifest
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
 import android.content.ContentValues.TAG
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -39,13 +43,14 @@ import java.util.*
 //bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
 val handlerState = 0
 var bluetoothIn: Handler? = null
-const val REQUEST_ENABLE_BT = 1
 
 //bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
 private var lastReceivedValue: String? = null
 
 
 class blueFrag : Fragment() {
+
+    private val REQUEST_LOCATION_PERMISSION = 100
 
     //cccccccccccccccccccccccccccccccccccccccccccccccccc
     //BluetoothAdapter
@@ -57,10 +62,6 @@ class blueFrag : Fragment() {
     var mAddressDevices: ArrayAdapter<String>? = null
     var mNameDevices: ArrayAdapter<String>? = null
 
-    //private val MY_UUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
-    private val MY_UUID: UUID = m_myUUID
-    private val REQUEST_BLUETOOTH_PERMISSION = 1
-    private val REQUEST_BLUETOOTH_SCAN_PERMISSION = 3
 
 
     companion object {
@@ -86,6 +87,7 @@ class blueFrag : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blue, container, false)
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -117,7 +119,6 @@ class blueFrag : Fragment() {
         mBtAdapter =
             (requireContext().getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager).adapter
 
-
         //Permisos coneccted
         if (ContextCompat.checkSelfPermission(
                 requireContext(),
@@ -133,23 +134,10 @@ class blueFrag : Fragment() {
         }
 
         //Permiso scan
-        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED) {
-            // Si no se tiene el permiso concedido, se solicita.
-            ActivityCompat.requestPermissions(requireActivity(),
-                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
-                REQUEST_BLUETOOTH_SCAN_PERMISSION)
-        } else {
-            // Si ya se tiene el permiso concedido, se realiza la operación que requiere el permiso.
-            startDiscovery()
-        }
+// Verificar si el permiso de ubicación ya ha sido otorgado
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val permissionCheck = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_SCAN)
-            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.BLUETOOTH_SCAN), 1)
-            }
-        }
+
+        //--------------------------------------------------------
 
         //---------------------------Zona pruebas-------------------------------------------------
         btnDispBT.setOnClickListener {
@@ -236,7 +224,7 @@ class blueFrag : Fragment() {
         }
     }
 
-    /*private class ConnectedThread(socket: BluetoothSocket) : Thread() {
+    private class ConnectedThread(socket: BluetoothSocket) : Thread() {
         private val mmInStream: InputStream?
         private val mmOutStream: OutputStream?
 
@@ -267,113 +255,6 @@ class blueFrag : Fragment() {
             mmInStream = tmpIn
             mmOutStream = tmpOut
         }
-    }*/
+    }
     //eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-
-    private val PERMISSIONS = arrayOf(
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.BLUETOOTH,
-        Manifest.permission.BLUETOOTH_ADMIN,
-        Manifest.permission.BLUETOOTH_SCAN
-    )
-
-
-    private val REQUEST_CODE_PERMISSIONS = 1
-
-    private fun requestPermissionsIfNecessary() {
-        val permissionsToRequest = ArrayList<String>()
-        for (permission in PERMISSIONS) {
-            if (ContextCompat.checkSelfPermission(requireContext(), permission) !=
-                PackageManager.PERMISSION_GRANTED
-            ) {
-                permissionsToRequest.add(permission)
-            }
-        }
-        if (permissionsToRequest.isNotEmpty()) {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                permissionsToRequest.toTypedArray(),
-                REQUEST_CODE_PERMISSIONS
-            )
-        }
-    }
-
-    private fun startDiscovery() {
-        mBtAdapter.startDiscovery()
-        Toast.makeText(requireContext(), "Scanning for devices...", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        when (requestCode) {
-            REQUEST_BLUETOOTH_SCAN_PERMISSION -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // Si se concedió el permiso, se realiza la operación que requiere el permiso.
-                    startDiscovery()
-                } else {
-                    // Si no se concedió el permiso, se muestra un mensaje al usuario.
-                    Toast.makeText(requireContext(), "Bluetooth scan permission denied", Toast.LENGTH_SHORT).show()
-                }
-            }
-            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        }
-    }
-
-    private inner class ConnectedThread(private val mmSocket: BluetoothSocket) : Thread() {
-        private val mmInStream: InputStream = mmSocket.inputStream
-        private val mmOutStream: OutputStream = mmSocket.outputStream
-        private var running: Boolean = false
-        private val MESSAGE_READ = 1
-
-        val mHandler = Handler(Looper.getMainLooper()) { msg ->
-            when (msg.what) {
-                // handle different message types here
-                else -> false
-            }
-        }
-
-
-        override fun run() {
-            val buffer = ByteArray(1024) // buffer store for the stream
-
-            // Keep listening to the InputStream until an exception occurs
-            while (running) {
-                try {
-                    // Read from the InputStream
-                    val bytes = mmInStream.read(buffer)
-                    val incomingMessage = String(buffer, 0, bytes)
-
-                    // Send the obtained bytes to the UI activity
-                    // so that it can be displayed in the TextView
-                    mHandler.obtainMessage(MESSAGE_READ, bytes, -1, incomingMessage)
-                        .sendToTarget()
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                    break
-                }
-            }
-        }
-
-        /* Call this from the main activity to send data to the remote device */
-        fun write(bytes: ByteArray) {
-            try {
-                mmOutStream.write(bytes)
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-        }
-
-        /* Call this from the main activity to shutdown the connection */
-        fun cancel() {
-            try {
-                running = false
-                mmSocket.close()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-        }
-
-        init {
-            running = true
-        }
-    }
 }
--- app/src/main/java/com/example/rega9/view/temporizadorFrag.kt
+++ app/src/main/java/com/example/rega9/view/temporizadorFrag.kt
@@ -30,24 +30,15 @@ import java.util.*
 
 class TemporizadorFrag : Fragment() {
 
-    //cccccccccccccccccccccccccccccccccccccccccccccccccc
-    //BluetoothAdapter
-    private val PERMISSION_REQUEST_LOCATION = 1001
-
-    lateinit var mBtAdapter: BluetoothAdapter
-    private var MyConexionBT: ConnectedThread? = null
-    private val recDataString = StringBuilder()
-    var mAddressDevices: ArrayAdapter<String>? = null
-    var mNameDevices: ArrayAdapter<String>? = null
-
-    companion object {
-        var m_myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
-        var m_bluetoothSocket: BluetoothSocket? = null
-
-        var m_isConnected: Boolean = false
-        lateinit var m_address: String
-    }
-    //cccccccccccccccccccccccccccccccccccccccccccccccccc
+    private lateinit var startButton: Button
+    private lateinit var pauseButton: Button
+    private lateinit var stopButton: Button
+    private lateinit var timerTextView: TextView
+    private lateinit var timerEditText: EditText
+    private lateinit var countDownTimer: CountDownTimer
+    private var timeInMillis: Long = 0
+    private var isTimerRunning = false
+    private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,219 +52,102 @@ class TemporizadorFrag : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blue, container, false)
+        return inflater.inflate(R.layout.fragment_temporizador, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-        mAddressDevices = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1)
-        mNameDevices = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1)
+        // Inicializar vistas
+        startButton = view.findViewById(R.id.startButton)
+        pauseButton = view.findViewById(R.id.pauseButton)
+        stopButton = view.findViewById(R.id.stopButton)
+        timerEditText = view.findViewById(R.id.timerEditText)
+        timerTextView = view.findViewById(R.id.timerTextView)
+        //
+        // spinner = view.findViewById(R.id.timerSpinner)
+        mediaPlayer = MediaPlayer.create(requireContext(), RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+
+        // Configurar el adaptador del Spinner
+        val times = arrayOf("10 segundos", "5 minutos", "10 minutos", "20 minutos", "30 minutos")
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, times)
 
 
+        // Asignar clics de botón
+        startButton.setOnClickListener { startTimer() }
+        pauseButton.setOnClickListener { pauseTimer() }
+        stopButton.setOnClickListener { stopTimer() }
 
-        //-------------------Variables---------------------
-        //val btnEnviar = view.findViewById<Button>(R.id.btnEnviar)
-        //val edtNumero = view.findViewById<EditText>(R.id.edtNumero)
-        val btnDispBT = view.findViewById<Button>(R.id.btnDisBT)
 
-        val spinDisp = view.findViewById<Spinner>(R.id.spinDisp)
-        val btnConect = view.findViewById<Button>(R.id.bntConnect)
-        //val txtRecibe = view.findViewById<TextView>(R.id.txtRecibe)
-        //--------------------------------------------------
+    }
 
-        /*bluetoothIn = object : Handler(Looper.getMainLooper()) {
-            override fun handleMessage(msg: Message) {
-                if (msg.what == handlerState) {
-                    val readMessage = msg.obj as String
-
-                    recDataString.append(readMessage)
-
-                    val endOfLineIndex: Int = recDataString.indexOf("#")
-                    if (endOfLineIndex > 0) {
-
-                        // Log.i("recDataString -> -> ", recDataString.toString())
-                        val dataInPrint: String = recDataString.substring(0, endOfLineIndex)
-                        txtRecibe.setText("$dataInPrint")
-
-                        recDataString.delete(0, recDataString.length)
-                    }
-                }
-            }
-        }*/
-
-        //--------------------------------------------------
-        //Inicializacion del bluetooth adapter
-        mBtAdapter = (requireContext().getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager).adapter
-
-        //Checar si esta encendido o apagado
-        if (mBtAdapter == null) {
-            Toast.makeText(requireContext(), "Bluetooth no está disponible en este dipositivo", Toast.LENGTH_LONG).show()
-        } else {
-            Toast.makeText(requireContext(), "Bluetooth está disponible en este dispositivo", Toast.LENGTH_LONG).show()
-        }
-        //--------------------------------------------------
-
-        //Boton dispositivos emparejados
-        /*btnDispBT.setOnClickListener {
-
-            if (mBtAdapter.isEnabled) {
-
-                val pairedDevices: Set<BluetoothDevice>? = mBtAdapter?.bondedDevices
-                mAddressDevices!!.clear()
-                mNameDevices!!.clear()
-
-                pairedDevices?.forEach { device ->
-
-                    val deviceName = device.name
-                    val deviceHardwareAddress = device.address // MAC address
-                    mAddressDevices!!.add(deviceHardwareAddress)
-                    //........... EN ESTE PUNTO GUARDO LOS NOMBRE A MOSTRARSE EN EL COMBO BOX
-                    mNameDevices!!.add(deviceName)
-                    //mNameDevices!!.add(" \n Dispositivo: ${deviceName} , ${deviceHardwareAddress} \n---------------\n")
-                }
-
-                //ACTUALIZO LOS DISPOSITIVOS
-                spinDisp.setAdapter(mNameDevices)
-                //Toast.makeText(this, mNameDevices.toString(), Toast.LENGTH_LONG).show()
-
-            } else {
-                val noDevices = "Ningun dispositivo pudo ser emparejado"
-                mAddressDevices!!.add(noDevices)
-                mNameDevices!!.add(noDevices)
-                Toast.makeText(requireContext(), "Primero vincule un dispositivo bluetooth", Toast.LENGTH_LONG).show()
-            }
-        }*/
-
-        //---------------------------Zona pruebas-------------------------------------------------
-        btnDispBT.setOnClickListener {
-            if (mBtAdapter.isEnabled) {
-                // Verificar si tienes permisos de ubicación
-                if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-                    == PackageManager.PERMISSION_GRANTED) {
-                    // Tienes permisos, obtener dispositivos emparejados
-                    val pairedDevices: Set<BluetoothDevice>? = mBtAdapter?.bondedDevices
-                    mAddressDevices!!.clear()
-                    mNameDevices!!.clear()
-
-                    pairedDevices?.forEach { device ->
-                        val deviceName = device.name
-                        val deviceHardwareAddress = device.address // MAC address
-                        mAddressDevices!!.add(deviceHardwareAddress)
-                        mNameDevices!!.add(deviceName)
+    private fun startTimer() {
+        if (!isTimerRunning) {
+            val timeText = timerEditText.text.toString()
+            if (timeText.isNotEmpty()) {
+                val timeInSeconds = timeText.toInt() * 1000
+                countDownTimer = object : CountDownTimer(timeInSeconds.toLong(), 1000) {
+                    override fun onTick(millisUntilFinished: Long) {
+                        timeInMillis = millisUntilFinished
+                        updateTimerText()
                     }
 
-                    spinDisp.setAdapter(mNameDevices)
-                } else {
-                    // No tienes permisos, solicitarlos
-                    ActivityCompat.requestPermissions(requireActivity(),
-                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                        PERMISSION_REQUEST_LOCATION)
+                    override fun onFinish() {
+                        playAlarm()
+                        stopTimer()
+                    }
                 }
-            } else {
-                val noDevices = "Ningun dispositivo pudo ser emparejado"
-                mAddressDevices!!.add(noDevices)
-                mNameDevices!!.add(noDevices)
-                Toast.makeText(requireContext(), "Primero vincule un dispositivo bluetooth", Toast.LENGTH_LONG).show()
-            }
-        }
-
-
-
-
-
-
-
-
-
-        //---------------------------Zona pruebas-------------------------------------------------
-        //-----Boton conextar
-        btnConect.setOnClickListener {
-            try {
-                if (m_bluetoothSocket == null || !m_isConnected) {
-
-                    val IntValSpin = spinDisp.selectedItemPosition
-
-                    m_address = mAddressDevices!!.getItem(IntValSpin).toString()
-                    Toast.makeText(requireContext(),m_address,Toast.LENGTH_LONG).show()
-                    // Cancel discovery because it otherwise slows down the connection.
-                    mBtAdapter?.cancelDiscovery()
-                    val device: BluetoothDevice = mBtAdapter.getRemoteDevice(m_address)
-                    m_bluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(m_myUUID)
-                    m_bluetoothSocket!!.connect()
-                    MyConexionBT = ConnectedThread(m_bluetoothSocket!!)
-                    MyConexionBT!!.start()
-
-                }
-
-                Toast.makeText(requireContext(),"CONEXION EXITOSA",Toast.LENGTH_LONG).show()
-                Log.i("data", "CONEXION EXITOSA")
-
-            } catch (e: IOException) {
-                //connectSuccess = false
-                e.printStackTrace()
-                Toast.makeText(requireContext(),"ERROR DE CONEXION",Toast.LENGTH_LONG).show()
-                Log.i("data", "ERROR DE CONEXION")
-            }
-        }
-
-        /*btnEnviar.setOnClickListener{
-            if(edtNumero.text.toString().isEmpty()){
-                Toast.makeText(requireContext(), "El control no puede estar vacío", Toast.LENGTH_SHORT)
-            }else{
-                var mensaje_2: String = edtNumero.text.toString()
-                //MyConexionBT?.write(mensaje_2) // otra manera de enviar datos
-                sendCommand(mensaje_2) //pero con esta se envia de una manera mas rapida
-                Log.d(ContentValues.TAG, "<<<<<<<< --------------- ENVIANDO UN MENSAJE ------------------->>>>>>>>>")
-            }
-        }*/
-        //--------------------------------------------------
-        //ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-    }
-
-    //eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-    private fun sendCommand(input: String) {
-        if (m_bluetoothSocket != null) {
-            try{
-                m_bluetoothSocket!!.outputStream.write(input.toByteArray())
-            } catch(e: IOException) {
-                e.printStackTrace()
+                countDownTimer.start()
+                isTimerRunning = true
+                updateButtons()
             }
         }
     }
 
-    private class ConnectedThread(socket: BluetoothSocket) : Thread() {
-        private val mmInStream: InputStream?
-        private val mmOutStream: OutputStream?
-
-        override fun run() {
-            val buffer = ByteArray(256)
-            var bytes: Int
-
-            while (true) {
-                try {
-                    bytes = mmInStream!!.read(buffer)
-                    val readMessage = String(buffer, 0, bytes)
-                    Log.i("readMessage -> -> ", readMessage.toString())
-                    bluetoothIn?.obtainMessage(handlerState, bytes, -1, readMessage)?.sendToTarget()
-                } catch (e: IOException) {
-                    break
-                }
-            }
-        }
-
-        init {
-            var tmpIn: InputStream? = null
-            var tmpOut: OutputStream? = null
-            try {
-                tmpIn = socket.inputStream
-                tmpOut = socket.outputStream
-            } catch (e: IOException) {
-            }
-            mmInStream = tmpIn
-            mmOutStream = tmpOut
+    private fun pauseTimer() {
+        if (isTimerRunning) {
+            countDownTimer.cancel()
+            isTimerRunning = false
+            updateButtons()
         }
     }
-    //eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+
+    private fun stopTimer() {
+        if (isTimerRunning) {
+            countDownTimer.cancel()
+            isTimerRunning = false
+            timeInMillis = 0
+            updateTimerText()
+            updateButtons()
+        }
+    }
+
+    private fun updateTimerText() {
+        val minutes = (timeInMillis / 1000) / 60
+        val seconds = (timeInMillis / 1000) % 60
+        timerTextView.setText("%02d:%02d".format(minutes, seconds))
+    }
+
+    private fun updateButtons() {
+        startButton.isEnabled = !isTimerRunning
+        pauseButton.isEnabled = isTimerRunning
+        stopButton.isEnabled = isTimerRunning
+    }
+
+    private fun playAlarm() {
+        mediaPlayer = MediaPlayer.create(requireContext(), RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM))
+        mediaPlayer.isLooping = true // Corregir la configuración de isLooping
+        mediaPlayer.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (isTimerRunning) {
+            countDownTimer.cancel()
+        }
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.stop()
+        }
+        mediaPlayer.release()
+    }
 }
